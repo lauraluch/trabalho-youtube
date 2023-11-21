3e130930ea9d38210a748e2722211b11c76bf9fa
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-20 22:52:58 -0300

    feat(GetPlaylist): add function to extract id from the url

--- frontend/app/src/components/GetPlaylist/GetPlaylist.tsx
+++ frontend/app/src/components/GetPlaylist/GetPlaylist.tsx
@@ -2,20 +2,29 @@ import { useState } from 'react';
 import { FormContainer, Label, Input, Button} from "./styles";
 
 interface GetPlaylistProps {
-    onPlaylistSearch: (playlistId: string) => void;
+    onPlaylistSearch: (url: string) => void;
 }
+
+
 
 const GetPlaylist: React.FC<GetPlaylistProps> = ({ onPlaylistSearch }) => {
 
-  const [playlistId, setPlaylistId] = useState('');
+  const [playlistUrl, setPlaylistUrl] = useState('');
   const [buttonText, setButtonText] = useState('Pesquisar Playlist');
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  
+  const extractId = (url: string) => {
+    const match = url.match(/list=([^&]+)/);
+    return match ? match[1] : playlistUrl;
+  }
+
+  console.log(extractId(playlistUrl))
 
   const handleSavePlanet = async () => {
     try {
       setIsLoading(true);
-      await onPlaylistSearch(playlistId);
-      setPlaylistId('');
+      await onPlaylistSearch(extractId(playlistUrl));
+      setPlaylistUrl('');
     } catch (error) {
       console.error('Erro ao pesquisar playlist:', error);
     } finally {
@@ -26,11 +35,11 @@ const GetPlaylist: React.FC<GetPlaylistProps> = ({ onPlaylistSearch }) => {
   return (
     <FormContainer>
       <Label>
-        Id da playlist:
+        Link ou Id da playlist:
         <Input
           type="text"
-          value={playlistId}
-          onChange={(e) => setPlaylistId(e.target.value)}
+          value={playlistUrl}
+          onChange={(e) => setPlaylistUrl(e.target.value)}
         />
       </Label>
       <Button onClick={handleSavePlanet} disabled={isLoading}>
